8c34cdc63426ff3f51a9441d84d38215a42b9926
NageswaraRao A <nanakapa@its.jnj.com>
2015-09-27 21:14:14 +0530

    CFL-986 CCXML Status Handler for Callflows
    Updated Controller Integration Test classs

--- src/test/java/com/janssen/connectforlife/callflows/web/it/CallStatusControllerBundleIT.java
+++ src/test/java/com/janssen/connectforlife/callflows/web/it/CallStatusControllerBundleIT.java
@@ -3,12 +3,14 @@ package com.janssen.connectforlife.callflows.web.it;
 import com.janssen.connectforlife.callflows.domain.Call;
 import com.janssen.connectforlife.callflows.helper.CallHelper;
 import com.janssen.connectforlife.callflows.repository.CallDataService;
+import com.janssen.connectforlife.callflows.repository.CallFlowDataService;
 
 import org.motechproject.testing.osgi.container.MotechNativeTestContainerFactory;
 
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.ExamFactory;
@@ -33,13 +35,16 @@ public class CallStatusControllerBundleIT extends RESTControllerPaxIT {
     @Inject
     private CallDataService callDataService;
 
+    @Inject
+    private CallFlowDataService callFlowDataService;
+
     @Test
     public void shouldReturnStatusOkIfTheCallStatusUpdateIsSuccessful() throws Exception {
         //Given
         Call call = CallHelper.createOutboundCall();
         callDataService.create(call);
         //When
-        HttpGet httpGet = buildGetRequest("/status/" + call.getId().toString(), "status", "ANSWERED", "reason",
+        HttpGet httpGet = buildGetRequest("/callflows/status/" + call.getId().toString(), "status", "ANSWERED", "reason",
                                           "call answered");
         HttpResponse response = getHttpClient().execute(httpGet);
 
@@ -52,11 +57,17 @@ public class CallStatusControllerBundleIT extends RESTControllerPaxIT {
     public void shouldReturnStatusOkIfTheCallIDIsNotFoundInDatabase() throws Exception {
 
         //When
-        HttpGet httpGet = buildGetRequest("/status/22", "status", "ANSWERED", "reason", "call answered");
+        HttpGet httpGet = buildGetRequest("/callflows/status/22", "status", "ANSWERED", "reason", "call answered");
         HttpResponse response = getHttpClient().execute(httpGet);
 
         // Then
         assertNotNull(response);
         assertThat(response.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_OK));
+    }
+
+    @After
+    public void tearDown() {
+        callDataService.deleteAll();
+        callFlowDataService.deleteAll();
     }
 }
