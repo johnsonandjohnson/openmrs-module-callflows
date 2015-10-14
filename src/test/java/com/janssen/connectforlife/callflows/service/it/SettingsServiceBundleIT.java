3bf504b2072ca1864a4f9b3bb4113a1c27d8fe2d
NageswaraRao A <nanakapa@its.jnj.com>
2015-10-14 17:39:02 +0530

    CFL-1035  fix for callflows PAX IT test
     fix for callflows PAX IT test

--- src/test/java/com/janssen/connectforlife/callflows/service/it/SettingsServiceBundleIT.java
+++ src/test/java/com/janssen/connectforlife/callflows/service/it/SettingsServiceBundleIT.java
@@ -110,10 +110,10 @@ public class SettingsServiceBundleIT extends BasePaxIT {
         assertThat(yo.getName(), equalTo(Constants.CONFIG_YO));
         assertThat(yo.getOutgoingCallMethod(), equalTo(Constants.CONFIG_YO_METHOD));
         assertThat(yo.getOutgoingCallUriTemplate(), equalTo(Constants.CONFIG_YO_OUT_TEMPLATE));
-        assertThat(voxeo.getOutboundCallLimit(), equalTo(Constants.CONFIG_VOXEO_OUTBOUND_CALL_LIMIT));
-        assertThat(voxeo.getOutboundCallRetryAttempts(), equalTo(Constants.CONFIG_VOXEO_OUTBOUND_CALL_RETRY_ATTEMPTS));
-        assertThat(voxeo.getOutboundCallRetrySeconds(), equalTo(Constants.CONFIG_VOXEO_OUTBOUND_CALL_RETRY_SECONDS));
-        assertThat(voxeo.getCallAllowed(), equalTo(Constants.CONFIG_VOXEO_CAN_PLACE_OUTBOUND_CALL));
+        assertThat(yo.getOutboundCallLimit(), equalTo(Constants.CONFIG_YO_OUTBOUND_CALL_LIMIT));
+        assertThat(yo.getOutboundCallRetryAttempts(), equalTo(Constants.CONFIG_YO_OUTBOUND_CALL_RETRY_ATTEMPTS));
+        assertThat(yo.getOutboundCallRetrySeconds(), equalTo(Constants.CONFIG_YO_OUTBOUND_CALL_RETRY_SECONDS));
+        assertThat(yo.getCallAllowed(), equalTo(Constants.CONFIG_YO_CAN_PLACE_OUTBOUND_CALL));
     }
 
     @Test
