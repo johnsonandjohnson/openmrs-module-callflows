f07abdc823a484ea87b2a00175d010583d3f5d97
druchniewicz <druchniewicz@soldevelo.com>
2019-09-09 13:39:00 +0200

    OCALL-10: Removed unused import

--- api/src/main/java/org/openmrs/module/callflows/api/util/CallUtil.java
+++ api/src/main/java/org/openmrs/module/callflows/api/util/CallUtil.java
@@ -37,7 +37,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-//import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ClassUtils;
 import org.supercsv.io.CsvMapWriter;
