1aba676fc3d467c7072dd60c2415c1f581e02cc3
SHulyalk <shulyalk@its.jnj.com>
2021-12-01 12:41:49 +0100

    AGRE-1957 Swagger documentation changes

--- omod/src/main/java/org/openmrs/module/callflows/web/controller/CallFlowController.java
+++ omod/src/main/java/org/openmrs/module/callflows/web/controller/CallFlowController.java
@@ -1,5 +1,6 @@
 package org.openmrs.module.callflows.web.controller;
 
+import io.swagger.annotations.*;
 import org.openmrs.module.callflows.api.builder.CallFlowBuilder;
 import org.openmrs.module.callflows.api.builder.CallFlowResponseBuilder;
 import org.openmrs.module.callflows.api.contract.CallFlowRequest;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,79 +30,151 @@ import java.util.List;
  *
  * @author bramak09
  */
+@Api(
+    value = "CallFlow",
+    tags = {"REST API to manage call flows"})
 @Controller
 @RequestMapping("/callflows")
 public class CallFlowController extends RestController {
 
-    private static final String LOOKUP_BY_NAME_PREFIX = "By Name";
+  private static final String LOOKUP_BY_NAME_PREFIX = "By Name";
 
-    @Autowired
-    @Qualifier("callflows.callFlowService")
-    private CallFlowService callFlowService;
+  @Autowired
+  @Qualifier("callflows.callFlowService")
+  private CallFlowService callFlowService;
 
-    /**
-     * REST API to create a call flow if no duplicate (by name) exists
-     *
-     * @param callFlowRequest with name, description and raw fields
-     * @return a response contract of the created callflow
-     * @throws CallFlowAlreadyExistsException if a callflow by the same name already exists in the system
-     */
-    @RequestMapping(value = "/flows", method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public CallFlowResponse createFlow(@RequestBody CallFlowRequest callFlowRequest)
-            throws CallFlowAlreadyExistsException {
-        CallFlow callFlow = callFlowService.create(CallFlowBuilder.createFrom(callFlowRequest));
-        return CallFlowResponseBuilder.createFrom(callFlow);
+  /**
+   * REST API to create a call flow if no duplicate (by name) exists
+   *
+   * @param callFlowRequest with name, description and raw fields
+   * @return a response contract of the created callflow
+   * @throws CallFlowAlreadyExistsException if a callflow by the same name already exists in the
+   *     system
+   */
+  @ApiOperation(
+      value = "Create a call flow",
+      notes = "Create a call flow if no duplicate (by name) exists")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            code = HttpURLConnection.HTTP_OK,
+            message = "Successfully created a call flow"),
+        @ApiResponse(
+            code = HttpURLConnection.HTTP_CONFLICT,
+            message = "Call flow with the same name already exists"),
+        @ApiResponse(
+            code = HttpURLConnection.HTTP_BAD_REQUEST,
+            message = "Call flow name is not an alphanumeric")
+      })
+  @RequestMapping(value = "/flows", method = RequestMethod.POST)
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public CallFlowResponse createFlow(
+      @ApiParam(
+              name = "callFlowRequest",
+              value = "Call flow request with name, description and raw fields")
+          @RequestBody
+          CallFlowRequest callFlowRequest)
+      throws CallFlowAlreadyExistsException {
+    CallFlow callFlow = callFlowService.create(CallFlowBuilder.createFrom(callFlowRequest));
+    return CallFlowResponseBuilder.createFrom(callFlow);
+  }
+
+  /**
+   * REST API to update a call flow
+   *
+   * @param callFlowRequest with name, description, status and raw fields
+   * @return a response contract of the updated callflow
+   * @throws CallFlowAlreadyExistsException if a callflow by the same name already exists in the
+   *     system
+   */
+  @ApiOperation(value = "Update a call flow", notes = "Update a call flow")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            code = HttpURLConnection.HTTP_OK,
+            message = "Successfully updated a call flow"),
+        @ApiResponse(
+            code = HttpURLConnection.HTTP_CONFLICT,
+            message = "Call flow with the same name already exists"),
+        @ApiResponse(
+            code = HttpURLConnection.HTTP_BAD_REQUEST,
+            message = "Call flow name is not an alphanumeric")
+      })
+  @RequestMapping(value = "/flows/{id}", method = RequestMethod.PUT)
+  @ResponseStatus(HttpStatus.OK)
+  @ResponseBody
+  public CallFlowResponse updateFlow(
+      @ApiParam(name = "id", value = "Call flow id to update", required = true)
+          @PathVariable(value = "id")
+          Long id,
+      @ApiParam(
+              name = "callFlowRequest",
+              value = "Call flow request with name, description and raw fields")
+          @RequestBody
+          CallFlowRequest callFlowRequest)
+      throws CallFlowAlreadyExistsException {
+    CallFlow callflow = CallFlowBuilder.createFrom(callFlowRequest);
+    callflow.setId(id != null ? id.intValue() : null);
+    return CallFlowResponseBuilder.createFrom(callFlowService.update(callflow));
+  }
+
+  /**
+   * REST API to delete a callflow
+   *
+   * @param id of callflow to delete
+   * @throws IllegalArgumentException if id is invalid
+   */
+  @ApiOperation(value = "Delete a call flow", notes = "Delete a call flow")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            code = HttpURLConnection.HTTP_OK,
+            message = "Successfully deleted a call flow"),
+        @ApiResponse(
+            code = HttpURLConnection.HTTP_BAD_REQUEST,
+            message = "Unsuccessful to delete a call flow")
+      })
+  @RequestMapping(value = "/flows/{id}", method = RequestMethod.DELETE)
+  @ResponseStatus(HttpStatus.OK)
+  public void deleteFlow(
+      @ApiParam(name = "id", value = "Call flow id to delete", required = true)
+          @PathVariable(value = "id")
+          Long id) {
+    callFlowService.delete(id != null ? id.intValue() : null);
+  }
+
+  /**
+   * REST API to search for callflows
+   *
+   * @param lookup a lookup term - currently only "By Name" is supported
+   * @param term a search term that is interpreted by the concerned lookup function invoked
+   * @return a list of found callflows
+   */
+  @ApiOperation(value = "Search for call flow", notes = "Search for call flow")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            code = HttpURLConnection.HTTP_OK,
+            message = "Successfully searches a call flow")
+      })
+  @RequestMapping(value = "/flows", method = RequestMethod.GET)
+  @ResponseBody
+  public SearchResponse searchFlowsByName(
+      @ApiParam(name = "lookup", value = "Lookup By Name") @RequestParam(value = "lookup")
+          String lookup,
+      @ApiParam(
+              name = "term",
+              value = "A search term that is interpreted by the concerned lookup function")
+          @RequestParam(value = "term")
+          String term) {
+    List<CallFlowResponse> callFlowResponses = new ArrayList<>();
+    if (LOOKUP_BY_NAME_PREFIX.equals(lookup)) {
+      List<CallFlow> callFlows = callFlowService.findAllByNamePrefix(term);
+      for (CallFlow callFlow : callFlows) {
+        callFlowResponses.add(CallFlowResponseBuilder.createFrom(callFlow));
+      }
     }
-
-    /**
-     * REST API to update a call flow
-     *
-     * @param callFlowRequest with name, description, status and raw fields
-     * @return a response contract of the updated callflow
-     * @throws CallFlowAlreadyExistsException if a callflow by the same name already exists in the system
-     */
-    @RequestMapping(value = "/flows/{id}", method = RequestMethod.PUT)
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public CallFlowResponse updateFlow(@PathVariable(value = "id") Long id, @RequestBody CallFlowRequest callFlowRequest)
-            throws CallFlowAlreadyExistsException {
-        CallFlow callflow = CallFlowBuilder.createFrom(callFlowRequest);
-        callflow.setId(id != null ? id.intValue() : null);
-        return CallFlowResponseBuilder.createFrom(callFlowService.update(callflow));
-    }
-
-    /**
-     * REST API to delete a callflow
-     *
-     * @param id of callflow to delete
-     * @throws IllegalArgumentException if id is invalid
-     */
-    @RequestMapping(value = "/flows/{id}", method = RequestMethod.DELETE)
-    @ResponseStatus(HttpStatus.OK)
-    public void deleteFlow(@PathVariable(value = "id") Long id) {
-        callFlowService.delete(id != null ? id.intValue() : null);
-    }
-
-    /**
-     * REST API to search for callflows
-     *
-     * @param lookup a lookup term - currently only "By Name" is supported
-     * @param term   a search term that is interpreted by the concerned lookup function invoked
-     * @return a list of found callflows
-     */
-    @RequestMapping(value = "/flows", method = RequestMethod.GET)
-    @ResponseBody
-    public SearchResponse searchFlowsByName(@RequestParam(value = "lookup") String lookup, @RequestParam(value = "term") String term) {
-        List<CallFlowResponse> callFlowResponses = new ArrayList<>();
-        if (LOOKUP_BY_NAME_PREFIX.equals(lookup)) {
-            List<CallFlow> callFlows = callFlowService.findAllByNamePrefix(term);
-            for (CallFlow callFlow : callFlows) {
-                callFlowResponses.add(CallFlowResponseBuilder.createFrom(callFlow));
-            }
-        }
-        return new SearchResponse(callFlowResponses);
-    }
-
+    return new SearchResponse(callFlowResponses);
+  }
 }
--- omod/src/main/java/org/openmrs/module/callflows/web/controller/CallStatusController.java
+++ omod/src/main/java/org/openmrs/module/callflows/web/controller/CallStatusController.java
@@ -1,5 +1,6 @@
 package org.openmrs.module.callflows.web.controller;
 
+import io.swagger.annotations.*;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -19,89 +20,106 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.net.HttpURLConnection;
 import java.util.Map;
 
 /**
- * This controller is used typically by a CCXML event handler to update the call status periodically. It can also be used by other IVR providers
+ * This controller is used typically by a CCXML event handler to update the call status
+ * periodically. It can also be used by other IVR providers
  *
  * @author nanakapa
  */
+@Api(
+    value = "Update the call status",
+    tags = {"REST API to update the call status periodically"})
 @Controller
 @RequestMapping("/callflows")
 public class CallStatusController extends RestController {
 
-    private static final Log LOGGER = LogFactory.getLog(CallStatusController.class);
+  private static final Log LOGGER = LogFactory.getLog(CallStatusController.class);
 
-    private static final String OK_RESPONSE = "";
+  private static final String OK_RESPONSE = "";
 
-    private static final String ERROR_RESPONSE = "error";
+  private static final String ERROR_RESPONSE = "error";
 
-    private static final String PARAM_STATUS = "status";
+  private static final String PARAM_STATUS = "status";
 
-    private static final String PARAM_REASON = "reason";
+  private static final String PARAM_REASON = "reason";
 
-    private static final String EXTERNAL_ID_PARAM_NAME = "externalIdParamName";
+  private static final String EXTERNAL_ID_PARAM_NAME = "externalIdParamName";
 
-    @Autowired
-    @Qualifier("callflows.callService")
-    private CallService callService;
+  @Autowired
+  @Qualifier("callflows.callService")
+  private CallService callService;
 
-    @Autowired
-    @Qualifier("callflows.callUtil")
-    private CallUtil callUtil;
+  @Autowired
+  @Qualifier("callflows.callUtil")
+  private CallUtil callUtil;
 
-    /**
-     * API to update the current call status
-     * IVR Providers like voxeo accept a string response for CCXML status handlers.
-     * The response is used to typically transition to another section of the CCXML document,
-     * and hence this status handler returns a string containing "error" in case of an error
-     * Sends out a call status event via the OpenMRS event system
-     *
-     * @param callId callId to lookup by
-     * @param params call status parameters
-     * @return blank response if the call status update is successful, otherwise returns a string containing error
-     */
-    @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(value = "/status/{callId}", method = RequestMethod.GET)
-    @ResponseBody
-    public String handleStatus(@PathVariable(value = "callId") String callId, @RequestParam Map<String, String> params) {
-        LOGGER.debug(String.format("handleStatus(callId=%s, params=%s", callId, params));
-        Call call = callService.findByCallId(callId);
-        if (null == call) {
-            return ERROR_RESPONSE;
-        }
-        // When ever we update the status, we always have to update the reason as well.
-        // convert to upper case in case the status received in lower case
-        CallStatus status = CallStatus.valueOf(params.get(PARAM_STATUS).toUpperCase());
-        call.setStatus(status);
+  /**
+   * API to update the current call status IVR Providers like voxeo accept a string response for
+   * CCXML status handlers. The response is used to typically transition to another section of the
+   * CCXML document, and hence this status handler returns a string containing "error" in case of an
+   * error Sends out a call status event via the OpenMRS event system
+   *
+   * @param callId callId to lookup by
+   * @param params call status parameters
+   * @return blank response if the call status update is successful, otherwise returns a string
+   *     containing error
+   */
+  @ApiOperation(value = "Update the current call status", notes = "Update the current call status")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            code = HttpURLConnection.HTTP_OK,
+            message = "Successfully updated the current call status")
+      })
+  @ResponseStatus(HttpStatus.OK)
+  @RequestMapping(value = "/status/{callId}", method = RequestMethod.GET)
+  @ResponseBody
+  public String handleStatus(
+      @ApiParam(name = "callId", value = "CallId to lookup by") @PathVariable(value = "callId")
+          String callId,
+      @ApiParam(name = "params", value = "Call status parameters") @RequestParam
+          Map<String, String> params) {
+    LOGGER.debug(String.format("handleStatus(callId=%s, params=%s", callId, params));
+    Call call = callService.findByCallId(callId);
+    if (null == call) {
+      return ERROR_RESPONSE;
+    }
+    // When ever we update the status, we always have to update the reason as well.
+    // convert to upper case in case the status received in lower case
+    CallStatus status = CallStatus.valueOf(params.get(PARAM_STATUS).toUpperCase());
+    call.setStatus(status);
 
-        String externalId = null;
-        if (!StringUtils.isBlank(params.get(EXTERNAL_ID_PARAM_NAME))) {
-            externalId = params.get(params.get(EXTERNAL_ID_PARAM_NAME));
-            LOGGER.debug(String.format("Reading external id value using: %s and value is : %s",
-                    params.get(EXTERNAL_ID_PARAM_NAME),
-                    externalId));
-        } else {
-            externalId = params.get(Constants.PARAM_EXTERNAL_ID);
-        }
-
-        call.setStatusText(params.get(PARAM_REASON));
-        // The Provider specific id and type, if passed then updated for this call record
-        if (validateExternalIdToUpdateCall(call.getExternalId(), externalId,
-                params.get(Constants.PARAM_EXTERNAL_TYPE))) {
-            call.setExternalId(externalId);
-            call.setExternalType(params.get(Constants.PARAM_EXTERNAL_TYPE));
-        }
-
-        // Update the call record with all the details
-        callService.update(call);
-        // There is a status change in the call, broadcast that across the system
-        callUtil.sendStatusEvent(call);
-        return OK_RESPONSE;
+    String externalId = null;
+    if (!StringUtils.isBlank(params.get(EXTERNAL_ID_PARAM_NAME))) {
+      externalId = params.get(params.get(EXTERNAL_ID_PARAM_NAME));
+      LOGGER.debug(
+          String.format(
+              "Reading external id value using: %s and value is : %s",
+              params.get(EXTERNAL_ID_PARAM_NAME), externalId));
+    } else {
+      externalId = params.get(Constants.PARAM_EXTERNAL_ID);
     }
 
-    private boolean validateExternalIdToUpdateCall(String dbExtId, String extId, String extType) {
-        return StringUtils.isBlank(dbExtId) && (StringUtils.isNotBlank(extId) && StringUtils.isNotBlank(extType));
+    call.setStatusText(params.get(PARAM_REASON));
+    // The Provider specific id and type, if passed then updated for this call record
+    if (validateExternalIdToUpdateCall(
+        call.getExternalId(), externalId, params.get(Constants.PARAM_EXTERNAL_TYPE))) {
+      call.setExternalId(externalId);
+      call.setExternalType(params.get(Constants.PARAM_EXTERNAL_TYPE));
     }
 
+    // Update the call record with all the details
+    callService.update(call);
+    // There is a status change in the call, broadcast that across the system
+    callUtil.sendStatusEvent(call);
+    return OK_RESPONSE;
+  }
+
+  private boolean validateExternalIdToUpdateCall(String dbExtId, String extId, String extType) {
+    return StringUtils.isBlank(dbExtId)
+        && (StringUtils.isNotBlank(extId) && StringUtils.isNotBlank(extType));
+  }
 }
